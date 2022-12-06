0541c3896aba122199aac286f7cf2ac0d81524fb
Sodeeq Elusoji <omoelu1@gmail.com>
2022-12-06 09:50:22 +0100

    add comment to spec file

--- cypress/e2e/product-spec.cy.js
+++ cypress/e2e/product-spec.cy.js
@@ -2,17 +2,22 @@ describe('Products testing', () => {
     it('should load the home page', () => {
         cy.visit('http://localhost:5000')
 
+        //verify that an h2 element contains the text 'Welcome to E-commerce testing with Cypress'
         cy.contains('h2', 'Welcome to E-commerce testing with Cypress')
     })
 
     it('should add a product to the cart and view the cart', () => {
+        //visit our product page
         cy.visit('http://localhost:5000/products.html')
         
+        // look for an element with this product-id and click the add to cart button
         cy.get('[data-product-id="abc123"]').click()
         cy.contains('Add to Cart').click()
 
+        // then go to the cart page
         cy.get('a[href="/cart.html"]').click()
 
+        // verify that the product we added to cart exists on the cart page
         cy.contains('h1', 'Shopping Cart')
         cy.contains('Product: abc123')
     })
